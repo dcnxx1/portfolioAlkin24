6b73f6461cc759aa24a2f7378a0d3c59f5b4b513
dcnxx1 <alkisali1@hotmail.com>
2024-04-07 23:17:41 +0200

    updates url paths

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,9 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  assetsInclude: ["**/*.m4v", "**/*.hevc"],
   resolve: {
-    alias: [{ find: "@", replacement: "/src" }],
+    alias: {
+      src: "./src",
+    },
   },
 });
